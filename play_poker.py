7fdf7e3ffedc9fada50b73a5c218eec35602a5ae
JENScoding <jens.lichter@posteo.de>
2020-05-09 03:26:50 +0200

    improved coding style; added arguments to functions; additional function for intermediate game status

--- play_poker.py
+++ play_poker.py
@@ -3,71 +3,96 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-# die folgenden Zeilen muessen je nach Spiel angepasst werden
-spieler = ("Spieler: ", "Anton", "Rafa", "Tico", "Alex", "Jens", "", "", "")
-
-n_spieler = 5
-offene_karten = 5
-
 # Passwoerter
-alle_p = [4, 8, 2, 9, 1, 6, 14, 7, 0, 3]
+alle_p = [4, 8, 2, 9, 1, 6, 14, 7, 0, 3, 16, 12, 11, 17, 18, 13]
 # Anton:
-a = alle_p[0]
-b = alle_p[1]
-# Rafa
-c = alle_p[2]
-d = alle_p[3]
+a1 = alle_p[0]
+b1 = alle_p[1]
+# Rafa:
+c1 = alle_p[2]
+d1 = alle_p[3]
 # Tico:
-e = alle_p[4]
-f = alle_p[5]
-# Alex
-g = alle_p[6]
-h = alle_p[7]
-# Jens
-i = alle_p[8]
-j = alle_p[9]
+e1 = alle_p[4]
+f1 = alle_p[5]
+# Alex:
+g1 = alle_p[6]
+h1 = alle_p[7]
+# Jens:
+i1 = alle_p[8]
+j1 = alle_p[9]
+# Johnny:
+k1 = alle_p[10]
+l1 = alle_p[11]
+# Jonas:
+m1 = alle_p[12]
+n1 = alle_p[13]
+# Quentin:
+o1 = alle_p[14]
+p1 = alle_p[15]
 
 
-def siehe_karten(Name_check, Name, Passwort1, Passwort2):
-    if (Name_check == spieler[1]):
-        if (Passwort1 == a) and (Passwort2 == b):
-            print(Name_check)
-            secret(Name)
+# plot, wenn Passwort korrekt
+def siehe_karten(name_check, name, passwort1, passwort2, spieler):
+    if name_check == spieler[1]:
+        if (passwort1 == a1) and (passwort2 == b1):
+            print(name_check)
+            secret(name)
         else:
             raise Exception("Falsches Passwort")
 
-    if (Name_check == spieler[2]):
-        if (Passwort1 == c) and (Passwort2 == d):
-            print(Name_check)
-            secret(Name)
+    if name_check == spieler[2]:
+        if (passwort1 == c1) and (passwort2 == d1):
+            print(name_check)
+            secret(name)
         else:
             raise Exception("Falsches Passwort")
 
-    if (Name_check == spieler[3]):
-        if (Passwort1 == e) and (Passwort2 == f):
-            print(Name_check)
-            secret(Name)
+    if name_check == spieler[3]:
+        if (passwort1 == e1) and (passwort2 == f1):
+            print(name_check)
+            secret(name)
         else:
             raise Exception("Falsches Passwort")
 
-    if (Name_check == spieler[4]):
-        if (Passwort1 == g) and (Passwort2 == h):
-            print(Name_check)
-            secret(Name)
+    if name_check == spieler[4]:
+        if (passwort1 == g1) and (passwort2 == h1):
+            print(name_check)
+            secret(name)
         else:
             raise Exception("Falsches Passwort")
 
-    if (Name_check == spieler[5]):
-        if (Passwort1 == i) and (Passwort2 == j):
-            print(Name_check)
-            secret(Name)
+    if name_check == spieler[5]:
+        if (passwort1 == i1) and (passwort2 == j1):
+            print(name_check)
+            secret(name)
+        else:
+            raise Exception("Falsches Passwort")
+
+    if name_check == spieler[6]:
+        if (passwort1 == k1) and (passwort2 == l1):
+            print(name_check)
+            secret(name)
+        else:
+            raise Exception("Falsches Passwort")
+
+    if name_check == spieler[7]:
+        if (passwort1 == m1) and (passwort2 == n1):
+            print(name_check)
+            secret(name)
+        else:
+            raise Exception("Falsches Passwort")
+
+    if name_check == spieler[8]:
+        if (passwort1 == o1) and (passwort2 == p1):
+            print(name_check)
+            secret(name)
         else:
             raise Exception("Falsches Passwort")
 
 
-# Karten und Funktionen
+# karten und Funktionen
 
-# erstelle Kartendeck
+# erstelle kartendeck
 muster = ["♠ ", "♦ ", "♥ ", "♣ "]
 zahl = np.arange(2, 11)
 zahl = list(zahl.astype(str))
@@ -79,15 +104,15 @@ for i in range(0, 4):
         karten.append(muster[i] + k)
 
 
-# sonstige Funktionen
+# Funktionen zum Zaehlen
 def pot(geld_einsatz):
-    pot = (geld_einsatz[0] + geld_einsatz[1] + geld_einsatz[2] + geld_einsatz[3]
-           + geld_einsatz[4] + geld_einsatz[5] + geld_einsatz[6] + geld_einsatz[7])
-    return int(pot)
+    t_pot = (geld_einsatz[0] + geld_einsatz[1] + geld_einsatz[2] + geld_einsatz[3]
+             + geld_einsatz[4] + geld_einsatz[5] + geld_einsatz[6] + geld_einsatz[7])
+    return int(t_pot)
 
 
-def sieger(sieger, bank, geld_einsatz, total_pot):
-    if (sieger == spieler[1]):
+def sieger(name, bank, geld_einsatz, total_pot, spieler):
+    if name == spieler[1]:
         bank[0] += total_pot
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -97,7 +122,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[2]):
+    elif name == spieler[2]:
         bank[0] -= geld_einsatz[0]
         bank[1] += total_pot
         bank[2] -= geld_einsatz[2]
@@ -107,7 +132,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[3]):
+    elif name == spieler[3]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] += total_pot
@@ -117,7 +142,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[4]):
+    elif name == spieler[4]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -127,7 +152,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[5]):
+    elif name == spieler[5]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -137,7 +162,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[5]):
+    elif name == spieler[5]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -147,7 +172,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[6]):
+    elif name == spieler[6]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -157,7 +182,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] -= geld_einsatz[6]
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[7]):
+    elif name == spieler[7]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -167,7 +192,7 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[6] += total_pot
         bank[7] -= geld_einsatz[7]
 
-    elif (sieger == spieler[8]):
+    elif name == spieler[8]:
         bank[0] -= geld_einsatz[0]
         bank[1] -= geld_einsatz[1]
         bank[2] -= geld_einsatz[2]
@@ -178,22 +203,23 @@ def sieger(sieger, bank, geld_einsatz, total_pot):
         bank[7] += total_pot
 
 
-def secret(Name):
+# Funktionen zum plotten
+def secret(name):
     fig, ax = plt.subplots()
 
-    # Karten definieren
-    Karte1 = beschriftung(Name[0])
-    Karte2 = beschriftung(Name[1])
+    # karten definieren
+    karte1 = beschriftung(name[0])
+    karte2 = beschriftung(name[1])
 
-    print("    ", Name[0], "    --    ", Name[1])
+    print("    ", name[0], "    --    ", name[1])
 
     # these are matplotlib.patch.Patch properties
     props = dict(boxstyle='round', facecolor='white')
 
     # place a text box in upper left in axes coords
-    ax.text(0.02, 0.95, Karte1, transform=ax.transAxes, fontsize=24,
+    ax.text(0.02, 0.95, karte1, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
-    ax.text(0.4, 0.95, Karte2, transform=ax.transAxes, fontsize=24,
+    ax.text(0.4, 0.95, karte2, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
     plt.axis("off")
     plt.show()
@@ -202,28 +228,30 @@ def secret(Name):
 def beschriftung(karte):
     teile = list(karte)
 
-    if (len(teile) == 3):
-        Karte = (
+    if len(teile) == 3:
+        karte = (
             f"{teile[2]} \n\n"
             f"    {teile[0]}    \n\n"
             f"         {teile[2]}"
         )
     else:
-        Karte = (
+        karte = (
             f"{teile[2] + teile[3]}\n\n"
             f"    {teile[0]}    \n\n"
             f"       {teile[2] + teile[3]}"
         )
-    return Karte
+    return karte
 
-part = 2 # nehm immer die zweite Liste (dient zur Verwirrung)
 
-def offenlegen(runde, karten_im_spiel):
+part = 2  # nehm immer die zweite Liste (dient zur Verwirrung)
+
+
+def offenlegen(runde, karten_im_spiel, n_spieler):
     breite = np.arange(0, len(karten_im_spiel[part]))
-    vergeben = np.array(alle_p)
+    vergeben = np.array(alle_p[0:(2 * n_spieler)])
     rest = np.isin(breite, vergeben)
-    for i in range(0, rest.size):
-        rest[i] = not rest[i]
+    for same in range(0, rest.size):
+        rest[same] = not rest[same]
     rest = list(breite[rest])
 
     nullte = "   "
@@ -235,80 +263,91 @@ def offenlegen(runde, karten_im_spiel):
 
     fig, ax = plt.subplots()
 
-    # Karten definieren
+    # karten definieren
     if runde == "null":
-        Karte1 = beschriftung(nullte)
-        Karte2 = beschriftung(nullte)
-        Karte3 = beschriftung(nullte)
-        Karte4 = beschriftung(nullte)
-        Karte5 = beschriftung(nullte)
+        karte1 = beschriftung(nullte)
+        karte2 = beschriftung(nullte)
+        karte3 = beschriftung(nullte)
+        karte4 = beschriftung(nullte)
+        karte5 = beschriftung(nullte)
 
-    if (runde == "flop"):
+    elif runde == "flop":
 
-        Karte1 = beschriftung(erste)
-        Karte2 = beschriftung(zweite)
-        Karte3 = beschriftung(dritte)
-        Karte4 = beschriftung(nullte)
-        Karte5 = beschriftung(nullte)
+        karte1 = beschriftung(erste)
+        karte2 = beschriftung(zweite)
+        karte3 = beschriftung(dritte)
+        karte4 = beschriftung(nullte)
+        karte5 = beschriftung(nullte)
         print("    ", erste, "   --   ", zweite,
               "   --   ", dritte)
-    elif (runde == "turn"):
-        Karte1 = beschriftung(erste)
-        Karte2 = beschriftung(zweite)
-        Karte3 = beschriftung(dritte)
-        Karte4 = beschriftung(vierte)
-        Karte5 = beschriftung(nullte)
+
+    elif runde == "turn":
+        karte1 = beschriftung(erste)
+        karte2 = beschriftung(zweite)
+        karte3 = beschriftung(dritte)
+        karte4 = beschriftung(vierte)
+        karte5 = beschriftung(nullte)
         print("    ", erste, "   --   ", zweite,
               "   --   ", dritte, "   --   ", vierte)
-    elif (runde == "river"):
-        Karte1 = beschriftung(erste)
-        Karte2 = beschriftung(zweite)
-        Karte3 = beschriftung(dritte)
-        Karte4 = beschriftung(vierte)
-        Karte5 = beschriftung(fuenfte)
+    elif runde == "river":
+        karte1 = beschriftung(erste)
+        karte2 = beschriftung(zweite)
+        karte3 = beschriftung(dritte)
+        karte4 = beschriftung(vierte)
+        karte5 = beschriftung(fuenfte)
         print("    ", erste, "   --   ", zweite,
               "   --   ", dritte, "   --   ", vierte,
               "    --    ", fuenfte)
+
+    else:
+        raise Exception("Wrong Argument. Use null, flop, turn or river ")
 
     # these are matplotlib.patch.Patch properties
     props = dict(boxstyle='round', facecolor='white')
 
     # place a text box in upper left in axes coords
-    ax.text(0, 0.95, Karte1, transform=ax.transAxes, fontsize=24,
+    ax.text(0, 0.95, karte1, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
-    ax.text(0.35, 0.95, Karte2, transform=ax.transAxes, fontsize=24,
+    ax.text(0.35, 0.95, karte2, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
-    ax.text(0.7, 0.95, Karte3, transform=ax.transAxes, fontsize=24,
+    ax.text(0.7, 0.95, karte3, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
-    ax.text(1.05, 0.95, Karte4, transform=ax.transAxes, fontsize=24,
+    ax.text(1.05, 0.95, karte4, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
-    ax.text(1.4, 0.95, Karte5, transform=ax.transAxes, fontsize=24,
+    ax.text(1.4, 0.95, karte5, transform=ax.transAxes, fontsize=24,
             verticalalignment='top', bbox=props)
     plt.axis("off")
     plt.show()
 
-# bestimme wer die Blinds hat und wer anfaengt
 
-def big_blind(bb):
-    if (n_spieler - bb < 0):
+# bestimme wer die Blinds hat und wer anfaengt
+def big_blind(bb, n_spieler):
+    if n_spieler - bb < 0:
         bb = 1
     return bb
 
-def beginn(b):
-    if (n_spieler - b < -1):
-        b = 2
-    elif (n_spieler - b < 0):
-        b = 1
-    return b
+
+def beginn(bb, n_spieler):
+    if n_spieler - bb < -1:
+        bb = 2
+    elif n_spieler - bb < 0:
+        bb = 1
+    return bb
+
 
 # zweiter Part von Liste alle_karten ist nur interessant. Der Rest
 # dient nur zur Verwirrung
-
-def alle_karten(seed):
+def alle_karten(seed, n_spieler, offene_karten):
     random.seed(seed)
-    alle_karten = [random.sample(karten, 15),
-                   random.sample(karten, n_spieler * 2 + offene_karten),
-                   random.sample(karten, 15)]
-    return alle_karten
+    spiel_karten_plus = [random.sample(karten, 15),
+                         random.sample(karten, n_spieler * 2 + offene_karten),
+                         random.sample(karten, 15)]
+    return spiel_karten_plus
 
 
+# printe aktuelle Kontostaende
+def aktueller_stand(spieler, bank, n_spieler):
+    stand = {}
+    for index in range(0, n_spieler):
+        stand[f"{spieler[index + 1]}"] = int(bank[index])
+    print(stand)
